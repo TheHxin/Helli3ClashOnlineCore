6b7e3b7cbd25506f72551035196c4e404715d7f6
Radin.h <122002544+TheHxin@users.noreply.github.com>
2023-01-24 22:20:57 +0330

    Server & Client

---
+++ Server/Server/Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
