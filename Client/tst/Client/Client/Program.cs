4ba8055d1dcd3cebf604176caa817f6968ae1fa5
Radin.h <122002544+TheHxin@users.noreply.github.com>
2023-01-25 23:25:32 +0330

    Client Code Basic IMP

---
+++ Client/tst/Client/Client/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            Client client = new Client();
+
+            client.start();
+            client.connecttoserver();
+
+            Console.ReadKey();
+        }
+    }
+    class Client
+    {
+        public static Client instance;
+        public static int databuffersize = 4096; //4 MB
+
+        public string ip = "127.0.0.1";
+        public int port = 38532;
+        public int muid = 0;
+        public TCP tcp;
+
+        public void start()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            tcp = new TCP();
+        }
+        public void connecttoserver()
+        {
+            tcp.connect();
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private NetworkStream stream;
+            private byte[] buffer;
+
+            public void connect()
+            {
+                socket = new TcpClient
+                {
+                    ReceiveBufferSize = databuffersize,
+                    SendBufferSize = databuffersize,
+                };
+
+                buffer = new byte[databuffersize];
+                socket.BeginConnect(instance.ip, instance.port, connectcallback, socket);
+            }
+            private void connectcallback(IAsyncResult _resault)
+            {
+                socket.EndConnect(_resault);
+                if (socket.Connected == false)
+                {
+                    return;
+                }
+
+                stream = socket.GetStream();
+                stream.BeginRead(buffer, 0, databuffersize, readcallback, null);
+            }
+            private void readcallback(IAsyncResult _resault)
+            {
+                try
+                {
+                    int _bytecount = stream.EndRead(_resault);
+                    if (_bytecount <= 0)
+                    {
+                        //disconnect
+                        return;
+                    }
+
+                    byte[] _data = new byte[_bytecount];
+                    Array.Copy(buffer, _data, _bytecount);
+
+                    //Data Hnadeling
+
+                    stream.BeginRead(buffer, 0, databuffersize, readcallback, null);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exaeption {e} occured !");
+                }
+            }
+        }
+    }
+}
