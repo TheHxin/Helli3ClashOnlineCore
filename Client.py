286e2d13899e900520d635c76aefa0eff739bc34
Radin.h <122002544+TheHxin@users.noreply.github.com>
2023-01-26 18:08:39 +0330

    Client code improved
    TODO: async client

--- Client.py
+++ Client.py
@@ -5,8 +5,10 @@ _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ("127.0.0.1",38532)
 _socket.connect(server_address)
 
-msg = "hi i am client"
-buffer = bytes(msg,"ascii")
-_socket.sendall(buffer)
-
-input()
+while True:
+    msg = input("Enter Message: ")
+    if msg == "exit":
+        _socket.close()
+        break
+    buffer = bytes(msg,"ascii")
+    _socket.sendall(buffer)
